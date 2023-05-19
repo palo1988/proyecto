9c988162af94be82b1b19caf0cb30150f52eaf04
palo1988 <133927725+palo1988@users.noreply.github.com>
2023-05-19 16:03:47 -0500

    Add files via upload

---
+++ TALLER_1/TALLER_1/vista/layouts/footer.php
@@ -0,0 +1,14 @@
+       <!-- /contenido-->
+    
+       </div> 
+
+</body>
+<style type="text/css">
+ p { color: blueviolet; }
+</style>
+<p>PROYECTO FINAL DESARROLLO DE SOFTWARE II </p>
+<p>CARLA REINOSO</p>
+<p>PABLO ROSERO </p>
+<p>JONATHAN BARROS</p>
+<p>CHRISTIAN NARANJO</p>
+</html>
---
+++ TALLER_1/TALLER_1/vista/layouts/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRODUCTOS TALLER 01 ITSQMET DS</title>
+    <link rel="stylesheet" href="vista/css/app.css">
+ 
+  
+</head>
+<body>
+
+   <div class="panel"><style type="text/css">
+ p { color: orangered; }
+</style>
+<p> INSTITUTO TECNOLOGICO SUPERIOR QUITO METROPOLITANO  </p>
+ITSQMET
+       <h1 class="text-center">MERCADO TUTI TALLER_1</h1>
+       <div> <align="center"><img width="200" src="imagenropa.jpg" ></div>
+       <h2 class="text-center">GESTIÓN DE PRODUCTOS VESTIMENTA</h2>
+
+       <!-- contenido-->
